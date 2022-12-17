39f0621f811d414f22690353c369dbe3e7df0fda
rogervc1 <vilcacariroger@gmail.com>
2022-12-17 09:35:27 -0500

    Primer avance del CRUD

---
+++ app/Controllers/Libros.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\Libro;
+class Libros extends Controller{
+    public function index(){
+        return view('libros/listar');
+    }
+}
